fa63752c7cb59c384343abcd60e564bf7ecfe6cc
mcoppola <mcoppola832@gmail.com>
2016-04-13 18:53:36 -0400

    removed video, swapped gif for new one

--- public/js/home.js
+++ public/js/home.js
@@ -1,27 +1,27 @@
 $(function(){
 
-    var $video = $('#fish'),
-        elm = $video[0]; 
-        playing = true;
-
-    $video.on('canplay', playVideo);
-
-    if (elm.readyState > 3) {
-      playVideo();
-    }
-
-    function playVideo() {
-        $('.ct-video').removeClass('ct-white-out');
-    }
-
-    $video.click(function(){
-        if (playing) {
-            $(this).get(0).pause()
-        } else {
-            $(this).get(0).play()
-        }
-        playing = !playing;
-    });
+    // var $video = $('#fish'),
+    //     elm = $video[0];
+    //     playing = true;
+    //
+    // $video.on('canplay', playVideo);
+    //
+    // if (elm.readyState > 3) {
+    //   playVideo();
+    // }
+    //
+    // function playVideo() {
+    //     $('.ct-video').removeClass('ct-white-out');
+    // }
+    //
+    // $video.click(function(){
+    //     if (playing) {
+    //         $(this).get(0).pause()
+    //     } else {
+    //         $(this).get(0).play()
+    //     }
+    //     playing = !playing;
+    // });
 
 
     var $tab        = $('.ct-signup-tab'),
